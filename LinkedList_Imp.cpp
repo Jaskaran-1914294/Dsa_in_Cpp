41f55ce3e573528204c88bd2a66941c8a4ef8228
Jaskaran <1914294.cse.coe@cgc.edu.in>
2021-05-20 17:36:32 +0530

    Finalized Important Questions

--- LinkedList_Imp.cpp
+++ LinkedList_Imp.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+//These are few important concept questions of linked lists
 
 class node{
  public:
@@ -22,7 +23,7 @@ void insert(node*&head,int val){
     }
     temp->next=n;
 }
-void show(node*temp){
+void display(node*temp){
 while(temp!=NULL){
     cout<<temp->data<<"->";
     temp=temp->next;
@@ -53,15 +54,40 @@ node* Recursive_rev(node*&head){
     head->next=NULL;
     return newhead;
 }
+//Reverse k-Nodes
+node* reverseK(node*&head,int k){
+    node*prev=NULL;
+    node*curr=head;
+    node*nextptr;
+    int count =0;
+    while(curr!=NULL && count<k){
+      nextptr=curr->next;
+      curr->next=prev;
+      prev=curr;
+      curr=nextptr;
+      count++;
+    }
+    if(nextptr!=NULL){
+        head->next=reverseK(nextptr,k);
+    }
+    return prev;
+
+}
 int main(){
 node*head=NULL;
 insert(head,1);
 insert(head,2);
 insert(head,3);
 insert(head,4);
-show(head);
+insert(head,5);
+insert(head,6);
+insert(head,7);
+insert(head,8);
+display(head);
 //node* newHead=Reverse(head);
-//show(newHead);
-node *byRec=Recursive_rev(head);
-show(byRec);
+//display(newHead);
+// node *byRec=Recursive_rev(head);
+// display(byRec);
+node*newHead=reverseK(head,2);
+display(newHead);
 }
