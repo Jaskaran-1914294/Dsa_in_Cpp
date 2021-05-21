f0652e96822febad0073affd828f149849d62892
Jaskaran <1914294.cse.coe@cgc.edu.in>
2021-05-21 22:09:25 +0530

    Linked list done

--- LinkedList_Imp.cpp
+++ LinkedList_Imp.cpp
@@ -73,6 +73,51 @@ node* reverseK(node*&head,int k){
     return prev;
 
 }
+//Creation of a loop 
+void createLoop(node*head,int pos){
+    int count=1;
+    node* n;
+    node*temp=head;
+    while(temp->next!=NULL){
+        if(count==pos){
+            n=temp;
+        }
+        temp=temp->next;
+        count++;
+    }
+    temp->next=n;
+
+}
+//Detection of a loop in a linked list= FLoyd's Algo
+bool checkLoop(node*head){
+    node*fast=head;
+    node*slow=head;
+    while(fast!=NULL && fast->next!=NULL){
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast){
+            return 1;
+        }
+    }
+    return 0;
+}
+//Deletion of a node
+void deleteLoop(node*&head){
+    node*fast=head;
+    node*slow=head;
+    do{
+        fast=fast->next->next;
+        slow=slow->next;
+    }while(fast!=slow);
+    fast=head;
+    while(fast->next!=slow->next){
+        fast=fast->next;
+        slow=slow->next;
+    }
+    slow->next=NULL;
+}
+
+
 int main(){
 node*head=NULL;
 insert(head,1);
@@ -83,11 +128,16 @@ insert(head,5);
 insert(head,6);
 insert(head,7);
 insert(head,8);
-display(head);
+// display(head);
 //node* newHead=Reverse(head);
 //display(newHead);
 // node *byRec=Recursive_rev(head);
 // display(byRec);
-node*newHead=reverseK(head,2);
-display(newHead);
+// node*newHead=reverseK(head,2);
+// display(newHead);
+createLoop(head,3);
+cout<<checkLoop(head)<<endl;
+deleteLoop(head);
+cout<<checkLoop(head);
+
 }
